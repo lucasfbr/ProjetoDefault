93d2425f8967d11e7610f9a82b06d849b0d2812e
Lucas Rosa <lucas-fbr@hotmail.com>
2017-02-06 17:05:14 -0200

    Inicio crud controle de acesso

--- app/Http/Controllers/Painel/RoleController.php
+++ app/Http/Controllers/Painel/RoleController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers\Painel;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Role;
 
 class RoleController extends Controller
 {
 
-    public function __construct()
+    private $role;
+
+    public function __construct(Role $role)
     {
         $this->middleware('auth');
+
+        $this->role = $role;
     }
 
     public function index(){
 
-        return view('painel.role.index');
+        $roles = $this->role->all();
+
+        return view('painel.role.index', ['roles' => $roles]);
 
     }
 
